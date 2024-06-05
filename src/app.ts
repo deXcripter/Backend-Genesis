782af7e28b256384fe3078c54023c03e54d8c5ca
Johnpaul Nnaji <indisputable.jay@gmail.com>
2024-06-05 10:02:25 +0100

    setting-up cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
+
+// client url
+const client = `http://localhost:${process.env.CLIENT_PORT || 3000}`;
 
 // middlewares
 app.use(helmet());
 app.use(express.json());
+app.use(cors({ origin: client, credentials: true }));
+
+// routes (add your routes here)
 
 export default app;
